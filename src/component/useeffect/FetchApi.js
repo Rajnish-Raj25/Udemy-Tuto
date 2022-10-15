f48cefc26e3b67486698e43aeb4520cb8cf563aa
Rajnish-Raj25 <rajnishraj268@gmail.com>
2022-10-15 11:54:25 +0530

    fetched github user and show data

--- src/component/useeffect/FetchApi.js
+++ src/component/useeffect/FetchApi.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./fetch.css";
 
 export default function FetchApi() {
   const [userData, setUserData] = useState([]);
@@ -7,11 +8,30 @@ export default function FetchApi() {
     const result = await fetch("https://api.github.com/users");
     const userData = await result.json();
     setUserData(userData);
-    console.log(userData);
+    // console.log(userData);
   };
   useEffect(() => {
     fetchData();
   }, []);
   console.log(userData);
-  return <>hello</>;
+  return (
+    <>
+      <div className="container">
+        {userData.map((curritem) => {
+          const { id, login, avatar_url, html_url } = curritem;
+          return (
+            <>
+              <div className="content" key={id}>
+                <img src={avatar_url} alt="profile" />
+                <div className="url">
+                  <h3>{login}</h3>
+                  <a href={html_url}>profile</a>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 }
